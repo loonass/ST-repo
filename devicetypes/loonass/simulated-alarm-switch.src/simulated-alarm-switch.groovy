e9d816ecea143016c4b8bded51f2b2f10b2ea033
Mike Harvey <loonass@yahoo.co.uk>
2018-10-17 22:31:01 +0100

    Simulated switch but customised for use as an Alarm Activate/Deactivate switch.

---
+++ devicetypes/loonass/simulated-alarm-switch.src/simulated-alarm-switch.groovy
@@ -0,0 +1,26 @@
+metadata {
+
+	definition (name: "Simulated Alarm Switch", namespace: "loonass", author: "Mike Harvey", runLocally: True,) {
+		capability "Switch"
+	}
+
+	tiles {
+		standardTile("switch", "device.switch", width: 3, height: 2) {
+			state "off", label: "Disarmed", action: "switch.on", icon: "st.Transportation.transportation13", backgroundColor: "#bc2323"
+			state "on", label: "Armed", action: "switch.off", icon: "st.Transportation.transportation12", backgroundColor: "#44b621"
+		}
+
+		main "switch"
+		details(["switch"])
+	}
+}
+
+def on() {
+	log.debug "Alarm on"
+	sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+	log.debug "Alarm off"
+	sendEvent(name: "switch", value: "off")
+}
