00a438ef8f794bc3b4b7eba33b4175147f759fa3
Mike Harvey <loonass@yahoo.co.uk>
2019-03-25 20:28:22 +0000

    minor changes

--- devicetypes/loonass/fibaro-fgbs-001-ubs.src/fibaro-fgbs-001-ubs.groovy
+++ devicetypes/loonass/fibaro-fgbs-001-ubs.src/fibaro-fgbs-001-ubs.groovy
@@ -24,13 +24,9 @@ metadata {
     command "createChildTempDevices"
     command "updateCurrentParams"
     command "listCurrentParams"
-    command "open1"
-    command "open2"
-    command "close1"
-    command "close2"
     
-    attribute "contact1","enum",["open1","close1"]
-    attribute "contact2","enum",["open2","close2"]
+    attribute "contact1","enum",["open","close"]
+    attribute "contact2","enum",["open","close"]
 	
 	fingerprint type: "2001", cc: "30 60 85 8E 72 70 86 7A", ccOut: "2B"
 }
@@ -48,52 +44,7 @@ tiles(scale: 2) {
 		state "closed", label:'secure',    icon:"st.locks.lock.locked",   backgroundColor:"#00a0dc"
 	}
 	
-	standardTile("temp1text", "temp1text", inactiveLabel: false, decoration: "flat", width: 4, height: 2) {
-		state "default", label:'Temp 1:', action:"", icon:""
-	}
-	standardTile("temp2text", "temp2text", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-		state "default", label:'Temp 2:', action:"", icon:""
-	}
-	standardTile("temp3text", "temp3text", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-		state "default", label:'Temp 3:', action:"", icon:""
-	}
-	standardTile("temp4text", "temp4text", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-		state "default", label:'Temp 4:', action:"", icon:""
-	}
 	valueTile("temperature1", "temperature1", width:2, height:2, decoration: "flat", canChangeIcon: true) {
-		state "default", label:'${currentValue}°', unit:"C", backgroundColors:[
-							[value: 0, color: "#153591"],
-							[value: 7, color: "#1e9cbb"],
-							[value: 15, color: "#90d2a7"],
-							[value: 23, color: "#44b621"],
-							[value: 28, color: "#f1d801"],
-							[value: 35, color: "#d04e00"],
-							[value: 37, color: "#bc2323"]
-                            ]
-    }
-	valueTile("temperature2", "temperature2", width:1, height:1, decoration: "flat") {
-		state "default", label:'${currentValue}°', unit:"C", backgroundColors:[
-							[value: 0, color: "#153591"],
-							[value: 7, color: "#1e9cbb"],
-							[value: 15, color: "#90d2a7"],
-							[value: 23, color: "#44b621"],
-							[value: 28, color: "#f1d801"],
-							[value: 35, color: "#d04e00"],
-							[value: 37, color: "#bc2323"]
-                            ]
-    }
-	valueTile("temperature3", "temperature3", width:1, height:1, decoration: "flat") {
-		state "default", label:'${currentValue}°', unit:"C", backgroundColors:[
-							[value: 0, color: "#153591"],
-							[value: 7, color: "#1e9cbb"],
-							[value: 15, color: "#90d2a7"],
-							[value: 23, color: "#44b621"],
-							[value: 28, color: "#f1d801"],
-							[value: 35, color: "#d04e00"],
-							[value: 37, color: "#bc2323"]
-                            ]
-    }
-    valueTile("temperature4", "temperature4", width:1, height:1, decoration: "flat") {
 		state "default", label:'${currentValue}°', unit:"C", backgroundColors:[
 							[value: 0, color: "#153591"],
 							[value: 7, color: "#1e9cbb"],
@@ -112,24 +63,12 @@ tiles(scale: 2) {
 		state "default", label:'List Config', action:"listCurrentParams"
 	}
     
-    standardTile("createchildren", "createchildren", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-		state "default", label:'Create Contact Children', action:"createChildDevices"
-	}
-    standardTile("createtempchildren", "createtempchildren", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-		state "default", label:'Create Temperature Children', action:"createChildTempDevices"
-	}
-    standardTile("removechildren", "removechildren", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-		state "default", label:'Remove Child Devices', action:"removeChildDevices"
-	}
 }
 
 main(["contact1"]) //, "temperature1"
 details([
-		//"temp1text", 
-        //"temp2text", "temperature2",
-        //"temp3text", "temperature3", "temp4text", "temperature4",
         "contact1", "temperature1", "contact2"
-        //"configure", "report", "createchildren", "createtempchildren", "removechildren"
+        //"configure", "report"
         ])
 
 preferences {
@@ -266,71 +205,15 @@ def updated() {
 	log.debug "updated()"
 	log.debug "Dont forget to press the button to send config to the device"
     //configure()
-    //createChildDevices()
+
 }
 def uninstalled() {
     log.debug "uninstalled()"
-    removeChildDevices()
 }
 
 def configure() {
 	log.debug "configure()"
     updateCurrentParams()
-}
-
-def createChildDevices(){
-	log.debug "Adding Child Devices if not already added"
-    for (i in 1..2) {
-    	try {
-        	log.debug "Trying to create child switch if it doesn't already exist ${i}"
-            def currentchild = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep${i}"}
-            if (currentchild == null) {
-            	log.debug "Creating child for ep${i}"
-				addChildDevice("smartthings", "Open/Closed Sensor", "${device.deviceNetworkId}-ep${i}", device.hub.id,
-                	[completedSetup: true, name: "${device.displayName} (Contact${i})", isComponent: false]) //, label: "${device.displayName} (Contact${i})"
-                /*addChildDevice(deviceHandlerName, "${device.deviceNetworkId}-${deviceName}${deviceNumber}", null,
-         			[completedSetup: true, label: "${device.displayName} (${deviceName}${deviceNumber})", 
-                	isComponent: false, componentName: "${deviceName}${deviceNumber}", componentLabel: "${deviceName} ${deviceNumber}"])*/
-            }
-        } catch (e) {
-            log.debug "Error adding child ${i}: ${e}"
-        }
-    }
-}
-def createChildTempDevices() {
-    log.debug "Creating Temperature children"
-    for (i in 1..4) {
-    	try {
-        	//If we have a temperature reading from this sensor (1 to 4) then try to create a child for it
-        	if (device.currentValue("temperature${i}") != null) {
-            	log.debug "Have received temperature readings for termperature${i} so creating a child for it if not already there"
-                def currentchild = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-temperature${i}"}
-                if (currentchild == null) {
-                    addChildDevice("smartthings", "Temperature Sensor", "${device.deviceNetworkId}-temperature${i}", device.hub.id,
-                        [completedSetup: true, name: "${device.displayName} (Temp${i})", isComponent: false]) //, label: "${device.displayName} (Temp${i})"
-                }
-            }
-            else {
-            	log.debug "No temperature received for temperature${i} so no child will be created" 
-            }
-        } catch (e) {
-            log.debug "Error adding Temperature # ${i} child: ${e}"
-        }
-    }
-}
-private removeChildDevices() {
-	log.debug "Removing Child Devices"
-    try {
-        getChildDevices()?.each {
-        	try {
-            	deleteChildDevice(it.deviceNetworkId)
-            } catch (e) {
-                log.debug "Error deleting ${it.deviceNetworkId}, probably locked into a SmartApp: ${e}"
-            }
-        }
-    } catch (err) {
-        log.debug "Either no children exist or error finding child devices for some reason: ${err}"
-    }
 }
 
 def parse(String description) {
@@ -377,15 +260,7 @@ def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) {
     	currentstate = "closed"
 	}
     createEvent(name: "contact1", value: currentstate, descriptionText: "${device.displayName} is ${currentstate}")
-    try {
-        def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep1"}
-        if (childDevice)
-        	def motionstate = (currentstate=="closed") ? "active" : "inactive"
-        	childDevice.sendEvent(name: "motion", value: motionstate)
-            childDevice.sendEvent(name: "contact", value: currentstate)
-    } catch (e) {
-        log.error "Couldn't find child device, probably doesn't exist...? Error: ${e}"
-    }
+    
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
@@ -403,15 +278,7 @@ def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap 
         //First update tile on this device
         sendEvent(name: "contact${cmd.sourceEndPoint}", value: currentstate, descriptionText: "$device.displayName - ep${cmd.sourceEndPoint} is ${currentstate}")
 		//If not null then we have found either ep1 or ep2, hence try to send to the child device aswell
-        try {
-            def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep${cmd.sourceEndPoint}"}
-            if (childDevice)
-                def motionstate = (currentstate=="closed") ? "active" : "inactive"
-                childDevice.sendEvent(name: "motion", value: motionstate)
-                childDevice.sendEvent(name: "contact", value: currentstate)
-        } catch (e) {
-            log.error "Couldn't find child device, probably doesn't exist...? Error: ${e}"
-        }
+        
     }
 	else if (cmd.commandClass == 49) {
 		if ((cmd.sourceEndPoint >= 3) && (cmd.sourceEndPoint <= 6)) {
@@ -434,16 +301,7 @@ def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap 
             
             sendEvent(name: tempendpoint, value: tempprocessed, displayed: true) //unit: getTemperatureScale()
             
-			//If not null then we have found either contact1 or contact2, hence try to send to the child
-            try {
-                def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-${tempendpoint}"}
-                if (childDevice)
-                	//We found a child device that matches so send it the new temperature
-                    childDevice.sendEvent(name: "temperature", value: tempprocessed)
-            } catch (e) {
-            	//Not an error message here as people may not want child temperature devices
-                log.debug "Couldn't find child ${tempendpoint} device, probably doesn't exist...? Error: ${e}"
-            }
+			
         }
 	}
 	else {
@@ -527,58 +385,6 @@ def listCurrentParams() {
 	cmds << zwave.configurationV1.configurationGet(parameterNumber: 13).format()
 	cmds << zwave.configurationV1.configurationGet(parameterNumber: 14).format()
 	delayBetween(cmds, 500)
-}
-
-def open1() {
-    sendEvent(name: "contact1", value: "open", descriptionText: "$device.displayName (1) is opened manually")
-    try {
-        def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep1"}
-        log.debug "Changing child ${childDevice} to open/inactive"
-        if (childDevice)
-        	childDevice.sendEvent(name: "motion", value: "inactive")
-            childDevice.sendEvent(name: "contact", value: "open")
-    } catch (e) {
-        log.error "Couldn't find child device, probably doesn't exist...? Error: ${e}"
-    }
-}
-
-def close1() {
-    sendEvent(name: "contact1", value: "closed", descriptionText: "$device.displayName (1) is closed manually")
-    try {
-        def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep1"}
-        log.debug "Changing child ${childDevice} to closed/active"
-        if (childDevice)
-        	childDevice.sendEvent(name: "motion", value: "active")
-            childDevice.sendEvent(name: "contact", value: "closed")
-    } catch (e) {
-        log.error "Couldn't find child device, probably doesn't exist...? Error: ${e}"
-    }
-}
-
-def open2() {
-    sendEvent(name: "contact2", value: "open", descriptionText: "$device.displayName (2) is opened manually")
-    try {
-        def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep2"}
-        log.debug "Changing child ${childDevice} to open/inactive"
-        if (childDevice)
-        	childDevice.sendEvent(name: "motion", value: "inactive")
-            childDevice.sendEvent(name: "contact", value: "open")
-    } catch (e) {
-        log.error "Couldn't find child device, probably doesn't exist...? Error: ${e}"
-    }
-}
-
-def close2() {
-    sendEvent(name: "contact2", value: "closed", descriptionText: "$device.displayName (2) is closed manually")
-    try {
-        def childDevice = getChildDevices()?.find { it.deviceNetworkId == "${device.deviceNetworkId}-ep2"}
-        log.debug "Changing child ${childDevice} to closed/active"
-        if (childDevice)
-        	childDevice.sendEvent(name: "motion", value: "active")
-			childDevice.sendEvent(name: "contact", value: "closed")
-    } catch (e) {
-        log.error "Couldn't find child device, probably doesn't exist...? Error: ${e}"
-    }
 }
 
 def ping() {
